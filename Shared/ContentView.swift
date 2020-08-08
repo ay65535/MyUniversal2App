b31876966fe489c8bcf5a9e371510ce60b7ca5d1
ay65535 <ay65535@icloud.com>
2020-08-08 20:38:28 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  
+//
+//  Created by ay65535 on 2020/08/08
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MyUniversal2AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyUniversal2AppApp.swift
+//  
+//
+//  Created by ay65535 on 2020/08/08
+//
+
+import SwiftUI
+
+@main
+struct MyUniversal2AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
